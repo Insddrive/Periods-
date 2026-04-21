c1a6b3488d7a690ffa06655fb891e0383e614078
Inderjeet Singh Talwandi Sabo <ins.ddrive@gmail.com>
2026-04-22 03:38:57 +0530

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,68 @@
+const CACHE_NAME = 'health-tracker-v1';
+const APP_SHELL = [
+  './',
+  './index.html'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(APP_SHELL)).catch(() => {})
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(
+        keys.map((key) => {
+          if (key !== CACHE_NAME) return caches.delete(key);
+        })
+      )
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (event) => {
+  const { request } = event;
+
+  if (request.mode === 'navigate') {
+    event.respondWith(
+      fetch(request)
+        .then((response) => {
+          const copy = response.clone();
+          caches.open(CACHE_NAME).then((cache) => cache.put('./', copy)).catch(() => {});
+          return response;
+        })
+        .catch(() => caches.match('./').then((res) => res || caches.match('./index.html')))
+    );
+    return;
+  }
+
+  if (new URL(request.url).origin === self.location.origin) {
+    event.respondWith(
+      caches.match(request).then((cached) => {
+        return (
+          cached ||
+          fetch(request).then((response) => {
+            const copy = response.clone();
+            caches.open(CACHE_NAME).then((cache) => cache.put(request, copy)).catch(() => {});
+            return response;
+          })
+        );
+      })
+    );
+    return;
+  }
+
+  event.respondWith(
+    fetch(request)
+      .then((response) => {
+        const copy = response.clone();
+        caches.open(CACHE_NAME).then((cache) => cache.put(request, copy)).catch(() => {});
+        return response;
+      })
+      .catch(() => caches.match(request))
+  );
+});
