ff398812e0654ceaa5e7ec9b61306ced7f3bfef3
Inderjeet Singh Talwandi Sabo <ins.ddrive@gmail.com>
2026-05-28 08:25:36 +0530

    Update Firebase configuration and background message handling

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -2,20 +2,19 @@ importScripts('https://www.gstatic.com/firebasejs/9.0.0/firebase-app-compat.js')
 importScripts('https://www.gstatic.com/firebasejs/9.0.0/firebase-messaging-compat.js');
 
 firebase.initializeApp({
-  apiKey: "ਤੁਹਾਡੀ-api-key",
+  apiKey: "AIzaSyAZQSsBTBMR4dGBkyka5VhPGB0FZ3lVtjQ",
   projectId: "periods-pwa",
   messagingSenderId: "1037007369851",
-  appId: "1:1037007369851:web:f109f4e0eb5def1830d4d"
+  appId: "1:1037007369851:web:f109f4e0eb5def1830d4de"
 });
 
 const messaging = firebase.messaging();
 
-// ਇਹ ਫੰਕਸ਼ਨ ਉਦੋਂ ਚੱਲੇਗਾ ਜਦੋਂ ਐਪ ਬੰਦ ਹੋਵੇਗੀ
 messaging.onBackgroundMessage((payload) => {
   console.log('Background message received: ', payload);
   const notificationTitle = payload.notification.title;
   const notificationOptions = {
-    body: payload.notification.body,
+    body: payload.notification.body
   };
   self.registration.showNotification(notificationTitle, notificationOptions);
 });
